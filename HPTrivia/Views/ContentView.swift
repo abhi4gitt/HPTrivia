7f4e21f3dae15952f341675df5b049a977e58e33
abhi4gitt <raiabhishek631@gmail.com>
2026-02-09 19:09:49 +0530

    Add Audio in App with the Help of AVKit and play it in loop

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -6,55 +6,66 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
     @State private var animateViewsIn = false
     
     var body: some View {
         GeometryReader { geo in
-                    ZStack {
-                        Image(.hogwarts)
-                            .resizable()
-                            .frame(width: geo.size.width * 3, height: geo.size.height)
-                            .padding(.top, 3)
-                            .phaseAnimator([false, true]) { content, phase in
-                                content
-                                    .offset(x: phase ? geo.size.width / 1.1 : -geo.size.width / 1.1)
-                            } animation: { _ in
-                                    .linear(duration: 60)
-                            }
-                        
-                        VStack {
+            ZStack {
+                Image(.hogwarts)
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                    .phaseAnimator([false, true]) { content, phase in
+                        content
+                            .offset(x: phase ? geo.size.width / 1.1 : -geo.size.width / 1.1)
+                    } animation: { _ in
+                            .linear(duration: 60)
+                    }
+                
+                VStack {
+                    VStack {
+                        if animateViewsIn {
                             VStack {
-                                if animateViewsIn {
-                                    VStack {
-                                        Image(systemName: "bolt.fill")
-                                            .imageScale(.large)
-                                            .font(.largeTitle)
-                                        
-                                        Text("HP")
-                                            .font(.custom("PartyLetPlain", size: 70))
-                                            .padding(.bottom, -50)
-                                        
-                                        Text("Trivia")
-                                            .font(.custom("PartyLetPlain", size: 60))
-                                    }
-                                    .padding(.top, 70)
-                                    .transition(.move(edge: .top))
-                                }
+                                Image(systemName: "bolt.fill")
+                                    .imageScale(.large)
+                                    .font(.largeTitle)
+                                
+                                Text("HP")
+                                    .font(.custom("PartyLetPlain", size: 70))
+                                    .padding(.bottom, -50)
+                                
+                                Text("Trivia")
+                                    .font(.custom("PartyLetPlain", size: 60))
                             }
-                            .animation(.easeOut(duration: 0.7).delay(0.2), value: animateViewsIn)
-                            
-                            Spacer()
+                            .padding(.top, 70)
+                            .transition(.move(edge: .top))
                         }
                     }
-                    .frame(width: geo.size.width, height: geo.size.height)
+                    .animation(.easeOut(duration: 0.7).delay(0.2), value: animateViewsIn)
+                    
+                    Spacer()
                 }
-                .ignoresSafeArea()
-                .onAppear {
-                    animateViewsIn = true
-                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            animateViewsIn = true
+//            playAudio()
+        }
     }
+    
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
+    }
+    
 }
 
 #Preview {
